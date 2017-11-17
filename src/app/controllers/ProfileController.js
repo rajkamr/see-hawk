a79c4429343e3ff253aa8f3ec2a324b66cd63fc3
Rajkamal Ramalingam <rajkamal_r@trimble.com>
2017-11-17 15:05:43 +0530

    #Integrated API add page.

--- src/app/controllers/ProfileController.js
+++ src/app/controllers/ProfileController.js
@@ -1,26 +1,48 @@
-(function(){
+(function () {
 
   angular
     .module('app')
     .controller('ProfileController', [
+      '$state',
       ProfileController
     ]);
 
-  function ProfileController() {
+  function ProfileController($state) {
     var vm = this;
 
-    vm.user = {
+    function reqestTypeSelcted(reqestType) {
+      vm.addApi.requestType = requestType;
+    }
+
+    function minuteTypeSelcted(minuteType) {
+      vm.addApi.minuteType = minuteType;
+    }
+
+    function scheduleTypeSelcted(scheduleType) {
+      vm.addApi.scheduleType = scheduleType;
+    }
+
+    function saveAPIValue() {
+      console.log("saveAPIValue--------" );
+      $state.go('home.dashboard');
+    }
+
+
+    vm.addApi = {
       title: 'Admin',
-      email: 'contact@flatlogic.com',
-      firstName: '',
-      lastName: '' ,
-      company: 'FlatLogic Inc.' ,
-      address: 'Fabritsiusa str, 4' ,
-      city: 'Minsk' ,
-      state: '' ,
+      monitorName: "",
+      clientEmail: 'sample@seehawk.com',
+      groupName: '',
+      request: ["POST", "GET"],
+      requestType: "",
+      minuteType: "",
+      schedule: ["Minute"],
+      scheduleType:"Minute",
+      url: "https://postman-echo.com/time/unit?timestamp=2016-10-10&unit=day",
+      minute: ["Every 5 Minute", "Every 10 Minute", "Every 20 Minute"],
       biography: 'We are young and ambitious full service design and technology company. ' +
-      'Our focus is JavaScript development and User Interface design.',
-      postalCode : '220007'
+        'Our focus is JavaScript development and User Interface design.',
+      postalCode: '220007'
     };
   }
 
