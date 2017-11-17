1a7370f67a5266856b4c1d4e20fcd748045a6f09
Rameshkumar Veldurai <rameshkumar_veldurai@trimble.com>
2017-11-17 16:07:17 +0530

    Service changes

--- src/app/components/services/BackendService.js
+++ src/app/components/services/BackendService.js
@@ -9,7 +9,30 @@
     ]);
   
     function backEndService($q, $firebaseObject){
-  
+      var groupIconList = ["chart1.png", "chart2.png"];
+      var groupList = [
+        {
+            "id": 1,
+            "name": "Group 1",
+            "last_exec_time": "17/01/2017 09:00 AM",
+            "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
+            "allClear": true
+        },
+        {
+            "id": 2,
+            "name": "Group 2",
+            "last_exec_time": "17/01/2017 10:00 AM",
+            "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
+            "allClear": false
+        },
+        {
+            "id": 3,
+            "name": "Group 3",
+            "last_exec_time": "17/01/2017 10:30 AM",
+            "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
+            "allClear": true
+        }
+    ];
       function getGroups() {
         console.log("groups");
         var ref = firebase.database().ref("groups");
@@ -21,7 +44,9 @@
       }
   
       return {
-        getGroups: getGroups
+        getGroups: getGroups,
+        groupList: groupList,
+        groupIconList: groupIconList
       };
     }
   })();
--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -2,36 +2,12 @@
     angular
         .module('app')
         .controller('DashboardController', [
+            'backEndService',
             DashboardController
         ]);
 
-    function DashboardController() {
+    function DashboardController(backEndService) {
         var vm = this;
-        var groupIconList = ["chart1.png", "chart2.png"];
-        vm.groupList = [
-            {
-                "id": 1,
-                "name": "Group 1",
-                "last_exec_time": "17/01/2017 09:00 AM",
-                "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
-                "allClear": true
-            },
-            {
-                "id": 2,
-                "name": "Group 2",
-                "last_exec_time": "17/01/2017 10:00 AM",
-                "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
-                "allClear": false
-            },
-            {
-                "id": 3,
-                "name": "Group 3",
-                "last_exec_time": "17/01/2017 10:30 AM",
-                "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
-                "allClear": true
-            }
-        ];
-
-        vm.ran
+        vm.groupList = backEndService.groupList;
     }
 })();
