724b4f7beb59ffe110b48128a0561d194b43655f
Rajkamal Ramalingam <rajkamal_r@trimble.com>
2017-11-17 17:05:10 +0530

    save api values

--- src/app/controllers/ProfileController.js
+++ src/app/controllers/ProfileController.js
@@ -3,11 +3,11 @@
   angular
     .module('app')
     .controller('ProfileController', [
-      'navService', '$mdSidenav', '$mdBottomSheet', '$log', '$q', '$state', '$mdToast', 'backEndService',
+      'navService', '$mdSidenav', '$mdBottomSheet', '$log', '$q', '$state', '$mdToast', 'backEndService', 'tableService',
       ProfileController
     ]);
 
-  function ProfileController(navService, $mdSidenav, $mdBottomSheet, $log, $q, $state, $mdToast, backEndService) {
+  function ProfileController(navService, $mdSidenav, $mdBottomSheet, $log, $q, $state, $mdToast, backEndService, tableService) {
     var vm = this;
     vm.reqestTypeSelcted = reqestTypeSelcted;
     vm.minuteTypeSelcted = minuteTypeSelcted;
@@ -31,6 +31,7 @@
 
     function saveAPIValue() {
       console.log("saveAPIValue--------");
+
       var isGroupFound = false;
       for (var i = 0; i < backEndService.groupList.length; i++) {
         if (backEndService.groupList[i].name === vm.addApi.groupName) {
@@ -38,21 +39,32 @@
         }
       }
       var groupList = [];
-       groupList = backEndService.groupList;
+      groupList = backEndService.groupList;
 
       if (!isGroupFound) {
         var group = {
-            "id": 10001,
-            "name": vm.addApi.groupName,
-            "last_exec_time": "17/01/2017 09:00 AM",
-            "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
-            "allClear": true
-          };
-          
-          groupList.push(group);
+          "id": 10001,
+          "name": vm.addApi.groupName,
+          "last_exec_time": "17/01/2017 09:00 AM",
+          "image": backEndService.groupIconList[Math.floor(Math.random() * backEndService.groupIconList.length)],
+          "allClear": true
+        };
+        groupList.push(group);
       }
 
       backEndService.groupList = groupList;
+
+      var api = {
+        "apiName": vm.addApi.monitorName,
+        "groupName":vm.addApi.groupName,
+        "last_exc_status": true,
+        "last_exc_time": "11/17/2017 10:50 AM",
+        "last_failure_time": "11/17/2017 09:50 AM",
+        "schedule": vm.addApi.scheduleType,
+        "notification_email": vm.addApi.clientEmail
+      }
+
+      tableService.allTableData.push(api);
       $state.go('home.dashboard');
     }
 
