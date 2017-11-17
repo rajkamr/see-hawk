b2bce8a06c5a72101f2c22676f2472ca8419c936
Rameshkumar Veldurai <rameshkumar_veldurai@trimble.com>
2017-11-17 15:37:31 +0530

    Added image

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -7,27 +7,27 @@
 
     function DashboardController() {
         var vm = this;
-        var groupIconList = ["receipt", "view_stream", "group_work", "view_array", "bug_report"];
+        var groupIconList = ["chart1.png", "chart2.png"];
         vm.groupList = [
             {
                 "id": 1,
                 "name": "Group 1",
                 "last_exec_time": "17/01/2017 09:00 AM",
-                "icon": groupIconList[Math.floor(Math.random() * groupIconList.length)],
+                "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
                 "allClear": true
             },
             {
                 "id": 2,
                 "name": "Group 2",
                 "last_exec_time": "17/01/2017 10:00 AM",
-                "icon": groupIconList[Math.floor(Math.random() * groupIconList.length)],
+                "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
                 "allClear": false
             },
             {
                 "id": 3,
                 "name": "Group 3",
                 "last_exec_time": "17/01/2017 10:30 AM",
-                "icon": groupIconList[Math.floor(Math.random() * groupIconList.length)],
+                "image": groupIconList[Math.floor(Math.random() * groupIconList.length)],
                 "allClear": true
             }
         ];
